4a52836dfec237fd3e77b094c28f4523431c83bf
Artem Kudria <ak98nneon@gmail.com>
2019-02-19 01:01:18 +0200

    Add validation input city

--- src/App.js
+++ src/App.js
@@ -43,21 +43,30 @@ class App extends React.Component {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await api_url.json();
 
-      var sunset = data.sys.sunset;
-      if (sunset) {
+      if (data.cod !== "404") {
+        var sunset = data.sys.sunset;
         var date = new Date();
         date.setTime(sunset);
         var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-      }
 
-      this.setState({
-        temp: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        pressure: data.main.pressure,
-        sunset: sunset_date,
-        error: undefined
-      });
+        this.setState({
+          temp: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          pressure: data.main.pressure,
+          sunset: sunset_date,
+          error: undefined
+        });
+      } else {
+        this.setState({
+          temp: undefined,
+          city: undefined,
+          country: undefined,
+          pressure: undefined,
+          sunset: undefined,
+          error: "Enter valid city name!"
+        })
+      }
     } else {
       this.setState({
         temp: undefined,
