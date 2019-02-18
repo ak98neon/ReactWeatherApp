bcefccb9919f4e92050c46253e82c03419ded7ac
Artem Kudria <ak98nneon@gmail.com>
2019-02-19 00:50:15 +0200

    Update style form, weather info

--- src/components/form.js
+++ src/components/form.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import {Form, FormGroup, Label, FormText, Button, Input, FormFeedback} from 'reactstrap';
-import styled from 'styled-components';
+import {Form, Button, Input, FormFeedback} from 'reactstrap';
 import '../scss/Form.scss';
-
-const FormDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
 
 class FormWeather extends React.Component {
   render() {
     return(
-      <FormDiv onSubmit={this.props.weatherMethod}>
-        <Input
-          type="text"
-          name="city"
-          placeholder="City"
-          onChange={this.changeInputCity}
-          valid={this.props.city}
-          invalid={this.props.error}
-        />
-        <FormFeedback valid>Sweet! City Find!</FormFeedback>
-        <FormFeedback invalid>Enter valid city name!</FormFeedback>
+      <Form onSubmit={this.props.weatherMethod} className="main_form">
+        <div className="input_city">
+          <Input
+            type="text"
+            name="city"
+            placeholder="City"
+            onChange={this.changeInputCity}
+            valid={this.props.city}
+            invalid={this.props.error}
+          />
+          <FormFeedback valid>Sweet! City Find!</FormFeedback>
+          <FormFeedback invalid>Enter valid city name!</FormFeedback>
+        </div>
         <br/>
         <Button color="success" className="btn_submit_form">Get Weather</Button>
-      </FormDiv>
+      </Form>
     )
   }
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Alert } from 'reactstrap';
 
 const Weather = (props) => (
-  <>
+  <div className="weather_component">
     {props.city &&
-      <div>
+      <Alert color="success">
         <p>Place: {props.city}, {props.country}</p>
         <p>Temperature: {props.temp}</p>
         <p>Pressure: {props.pressure}</p>
         <p>Sunset: {props.sunset}</p>
-      </div>
+      </Alert>
     }
-  </>
+  </div>
 );
 
 export default Weather;
